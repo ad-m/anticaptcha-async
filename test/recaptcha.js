aa19777846ee9d8dab3fd27e289136a5ce36ef34
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-10-02 07:29:57 +0200

    Remove debug printout

--- test/recaptcha.js
+++ test/recaptcha.js
@@ -3,7 +3,6 @@ const { skipUntilEnv } = require('../lib.js');
 
 skipUntilEnv('ANTICAPTCHA_KEY', 'examples/recaptcha.js', async t => {
     const result = await require('../examples/recaptcha.js')();
-    console.log(result);
     t.true(result.includes('Verification Success... Hooray!'));
 });
 

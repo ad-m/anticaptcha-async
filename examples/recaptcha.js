1c30f63d32622c1c6bfba8e80a822a47cfb070a6
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-10-03 02:24:54 +0200

    Fix recaptcha example

--- examples/recaptcha.js
+++ examples/recaptcha.js
@@ -3,25 +3,26 @@ const request = require('superagent');
 
 const anticaptcha = require('../index');
 
+const legible_useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' +
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36';
+
 const main = async () => {
     const client = anticaptcha(process.env.ANTICAPTCHA_KEY);
 
     const url = 'https://www.google.com/recaptcha/api2/demo?invisible=false';
 
-    const agent = request.agent();
-    const resp = await agent.get(url);
+    const agent = request
+        .agent()
+        .set('User-Agent', legible_useragent);
 
+    const resp = await agent.get(url);
     const key = resp.text.match(/data-sitekey="(.+?)"/)[1];
 
     const result = await client.getRecaptcha(url, key);
-    const data = {
-        'g-recaptcha-response': result.getValue(),
-    };
-    console.dir(data, {depth: null});
     const submit_resp = await agent
         .post(url)
         .type('form')
-        .send(data);
+        .send({ 'g-recaptcha-response': result.getValue() });
     return submit_resp.text;
 };
 
